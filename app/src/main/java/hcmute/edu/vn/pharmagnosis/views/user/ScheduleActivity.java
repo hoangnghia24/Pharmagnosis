3b87e406d759f9c27b47167f00daf9568f80871a
HoangNghia <dqhn2403@gmail.com>
2026-03-29 01:19:48 +0700

    update view and layout  medicine schedule

--- app/src/main/java/hcmute/edu/vn/pharmagnosis/views/user/ScheduleActivity.java
+++ app/src/main/java/hcmute/edu/vn/pharmagnosis/views/user/ScheduleActivity.java
@@ -1,6 +1,17 @@
 package hcmute.edu.vn.pharmagnosis.views.user;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import hcmute.edu.vn.pharmagnosis.R;
 
 public class ScheduleActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // gọi giao diện XML
+        setContentView(R.layout.activity_medication_schedule);
+    }
+
 }
